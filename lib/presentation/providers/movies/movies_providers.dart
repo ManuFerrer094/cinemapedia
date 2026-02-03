a49c8f08fb162a85b7736d779728dc9806d4949d
Manuel Ferrer <manuferrer094@gmail.com>
2026-02-03 08:43:04 +0100

    correccion de errores pub get

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -1,43 +1,67 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
-import 'package:flutter_riverpod/legacy.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final nowPlayingMoviesProvider =
+    NotifierProvider<NowPlayingMoviesNotifier, List<Movie>>(
+      NowPlayingMoviesNotifier.new,
+    );
 
+final upcomingMoviesProvider =
+    NotifierProvider<UpcomingMoviesNotifier, List<Movie>>(
+      UpcomingMoviesNotifier.new,
+    );
 
-final nowPlayingMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
-  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
-  return MoviesNotifier(
-    fetchMoreMovies: fetchMoreMovies,
-  );
-});
-
-final upcomingMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
-  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getUpcoming;
-  return MoviesNotifier(
-    fetchMoreMovies: fetchMoreMovies,
-  );
-});
-
-final popularMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
-  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getPopular;
-  return MoviesNotifier(
-    fetchMoreMovies: fetchMoreMovies,
-  );
-});
+final popularMoviesProvider =
+    NotifierProvider<PopularMoviesNotifier, List<Movie>>(
+      PopularMoviesNotifier.new,
+    );
 
 typedef MovieCallback = Future<List<Movie>> Function({required int page});
 
-class MoviesNotifier extends StateNotifier<List<Movie>> {
+class NowPlayingMoviesNotifier extends Notifier<List<Movie>> {
   int currentPage = 0;
-  MovieCallback fetchMoreMovies;
 
-
-  MoviesNotifier({
-    required this.fetchMoreMovies,
-  }) : super([]);
+  @override
+  List<Movie> build() {
+    return [];
+  }
 
   Future<void> loadNextPage() async {
     currentPage++;
+    final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage);
+    state = [...state, ...movies];
+  }
+}
+
+class UpcomingMoviesNotifier extends Notifier<List<Movie>> {
+  int currentPage = 0;
+
+  @override
+  List<Movie> build() {
+    return [];
+  }
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+    final fetchMoreMovies = ref.watch(movieRepositoryProvider).getUpcoming;
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage);
+    state = [...state, ...movies];
+  }
+}
+
+class PopularMoviesNotifier extends Notifier<List<Movie>> {
+  int currentPage = 0;
+
+  @override
+  List<Movie> build() {
+    return [];
+  }
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+    final fetchMoreMovies = ref.watch(movieRepositoryProvider).getPopular;
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
   }
