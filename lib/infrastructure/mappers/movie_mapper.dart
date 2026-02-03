ff7c28d59eb372a8002a2c1467cfbfa001278dde
Manuel Ferrer <manuferrer094@gmail.com>
2026-02-03 11:03:48 +0100

    correccion mapeos y respuesta movies

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,49 +1,48 @@
-
-
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
-import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
-  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
-      adult: movieDB.adult,
-      backdropPath: (movieDB.backdropPath != null && movieDB.backdropPath != '') 
-        ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}' 
-        : 'no-poster',
-      genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
-      id: movieDB.id,
-      originalLanguage: movieDB.originalLanguage,
-      originalTitle: movieDB.originalTitle,
-      overview: movieDB.overview,
-      popularity: movieDB.popularity,
-      posterPath: (movieDB.posterPath != null && movieDB.posterPath != '') 
-        ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}' 
-        : 'no-poster',
-      releaseDate: movieDB.releaseDate,
-      title: movieDB.title,
-      video: movieDB.video,
-      voteAverage: movieDB.voteAverage,
-      voteCount: movieDB.voteCount,
-    );
-
-    static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
       adult: moviedb.adult,
-      backdropPath: (moviedb.backdropPath != '' && moviedb.backdropPath != '') 
-        ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}' 
-        : 'no-poster',
-      genreIds: moviedb.genres.map((e) => e.id.toString()).toList(),
+      backdropPath: (moviedb.backdropPath != '') 
+        ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+        : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
       id: moviedb.id,
       originalLanguage: moviedb.originalLanguage,
       originalTitle: moviedb.originalTitle,
       overview: moviedb.overview,
       popularity: moviedb.popularity,
-      posterPath: (moviedb.posterPath != '') 
-        ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}' 
-        : 'no-poster',
+      posterPath: (moviedb.posterPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
+        : 'https://www.movienewz.com/img/films/poster-holder.jpg',
+      releaseDate: moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount
+    );
+
+    static Movie movieDetailsToEntity( MovieDetails moviedb ) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '') 
+        ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+        : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+      genreIds: moviedb.genres.map((e) => e.name ).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
+        : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
       voteAverage: moviedb.voteAverage,
-      voteCount: moviedb.voteCount,
+      voteCount: moviedb.voteCount
     );
+
 }
